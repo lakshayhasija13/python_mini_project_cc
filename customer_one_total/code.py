8e48c3e57fa937733b38d212945b34bef63ff7c7
Lakshay Hasija <LAKSHAYHASIJA5991@GMAIL.COM>
2026-02-19 19:52:07 +0530

    Implement customer one total calculation
    This code calculates the total cost for customer one, including item descriptions and sales tax.

---
+++ customer_one_total/code.py
@@ -0,0 +1,60 @@
+# Task 1
+lovely_loveseat_description = "Stranger, if you passing meet me and desire to speak to me, why should you not speak to me? And why should I not speak to you?
+"
+
+# Task 2
+lovely_loveseat_price = 254.00
+
+# Task 3
+stylish_settee_description = "Stylish Settee. Faux leather on birch. 29.50 inches high x 54.75 inches wide x 28 inches deep. Black."
+
+# Task 4
+stylish_settee_price = 180.50
+
+# Task 5
+luxurious_lamp_description = "Luxurious Lamp. Glass and iron. 36 inches tall. Brown with cream shade."
+
+# Task 6
+luxurious_lamp_price = 52.15
+
+# Task 7
+sales_tax = 0.088
+
+# Task 8
+customer_one_total = 0
+
+# Task 9
+customer_one_itemization = ""
+
+# Task 10
+customer_one_total += lovely_loveseat_price
+
+# Task 11
+customer_one_itemization += lovely_loveseat_description
+
+# Task 12
+customer_one_total += luxurious_lamp_price
+
+# Task 13
+customer_one_itemization += luxurious_lamp_description
+
+# Task 14
+customer_one_tax = customer_one_total * sales_tax
+
+# Task 15
+customer_one_total += customer_one_tax
+
+# Task 16
+print('Customer One Items:')
+
+# Task 17
+print(customer_one_itemization)
+
+# Task 18
+print('Customer One Total:')
+
+# Task 19
+print(customer_one_total)
+
+# Task 20
+# Customer Total calculated successfully !

c922a07e88580ba5bf5b69c666154f7218042e5a
Lakshay Hasija <LAKSHAYHASIJA5991@GMAIL.COM>
2026-02-19 23:43:14 +0530

    Implement Magic 8-Ball functionality in code.py

---
+++ magic_8_ball/code.py
@@ -0,0 +1,46 @@
+# TASK 1
+name = "Mohan"
+
+# TASK 2
+question = "Will I win the lottery?"
+
+# TASK 3
+answer = ""
+
+# TASK 4
+import random
+
+# TASK 5
+random_number = random.randint(1, 10)
+
+# TASK 6
+if random_number == 1:
+  answer = 'Yes - definately'
+elif random_number == 2:
+  answer = 'It is decidedly so'
+elif random_number == 3:
+  answer = 'Without a doubt'
+elif random_number == 4:
+  answer = 'Reply hazy, try again'
+elif random_number == 5:
+  answer = 'Ask again later'
+elif random_number == 6:
+  answer = 'Better not tell you now'
+elif random_number == 7:
+  answer = 'My sources say no'
+elif random_number == 8:
+  answer = 'Outlook not so good'
+elif random_number == 9:
+  answer = 'Very doubt'
+# TASK 7
+else:
+  answer = 'Error'
+
+# TASK 8
+print(f'{name} asks: {question}')
+
+# TASK 9
+print(f'Magic 8-Ball\'s answer: {answer}')
+
+# TASK 10
+# PMagic 8 Ball successfully built
